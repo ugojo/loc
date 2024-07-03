e5bcc88239305dcb506f9a0088079c23cd9d3a7a
obi ugochukwu <obiugochukwu003@gmail.com>
2024-07-03 14:50:13 -0700

    update seven

--- controller/controller.js
+++ controller/controller.js
@@ -1,14 +1,27 @@
-var satelize = require('satelize');
+const axios = require('axios')
 // const ip = require('ip')
 
+const url = 'https://ipapi.co/json/'
 
 const getInfo = async (req, res)=>{
 
-    await satelize.satelize({ip: req.ip}, function(err, payload) {
+    visitor = req.query.visitor_name
 
-        console.log(payload);
-         res.json({'payload': payload})
-      });
+    await axios 
+     .get(url)
+    .then((response)=>{
+       const data = response.data
+
+       console.log(response.data);
+
+        res.status(200).json({"client_ip": data.ip,
+                              "location": data.country_name,
+                              "greeting": "Hello," + visitor + "! the temperature is 11 degees Celcius in " + data.country_name
+        })
+    })
+    .catch(err=>{
+        console.log({'error': err});
+    })
 
 }
 
