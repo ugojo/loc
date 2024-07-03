0f115713d779f70381826252572683231825fffa
obi ugochukwu <obiugochukwu003@gmail.com>
2024-07-03 13:01:07 -0700

    update one

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,19 +1,23 @@
 const express = require('express')
+const ip = require('ip')
 
 const router = express.Router()
 
 
 router.get('/',(req, res)=>{
+    const ipAddress = ip.address()
     const ipInfo = req.ipInfo
     const name = req.query
 
     res.status(200).json({'client_ip': ipInfo.ip, 
-              'location': ipInfo.country,
-              'greeting': `Hello, ${name}!,the temperature is 11 degrees celcius in ${ipInfo.country}`
+              'location': ipInfo.Country,
+              'greeting': `Hello, ${name}!,the temperature is 11 degrees celcius in ${ipInfo.Country}`
      })
 
     // console.log({'IP' : ipInfo});
     // console.log({'vistor name': name});
+    // console.log({'Ip address': ipInfo});
+    // console.log({'Ip Conutry': ip.country});
 })
 
 module.exports = router
