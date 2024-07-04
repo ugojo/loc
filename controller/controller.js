3b0af03c555673493b89409a0b57473336eb287e
obi ugochukwu <obiugochukwu003@gmail.com>
2024-07-04 01:09:46 -0700

    update 12

--- controller/controller.js
+++ controller/controller.js
@@ -9,7 +9,7 @@ async function getInfo(req, res) {
     try {
         const userIp = await axios.get(process.env.URL);
 
-        const {ip, region, latitude, longitude} = userIp.data
+        const {ip, country, region, latitude, longitude} = userIp.data
 
         const temps = await axios.get(`http://api.openweathermap.org/data/2.5/weather?` 
 			+`lat=${latitude}&lon=${longitude}&appid=${process.env.API_KEY}`)
@@ -17,11 +17,12 @@ async function getInfo(req, res) {
         const temp = temps.data.main.temp
 
         res.status(200).json({"client_ip": ip,
-                  "location": region,
+                  "location": region ,
+                  "Cont": country,
                   "greeting":  `Hello,${visitor}!, the temperature is ${temp} degrees celcius in ${region}`
         })
 
-        // console.log(userIp.data, currency, region);
+        // console.log(userIp.data, region);
     } catch (error) {
        return res.status(400).json({'error': error})
     }
